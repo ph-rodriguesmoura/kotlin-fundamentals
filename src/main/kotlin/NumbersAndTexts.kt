7998bc8d20519db2a0041c8826da86ad9b9dcfe7
Pedro Moura <ph.rodriguesmoura@gmail.com>
2023-05-22 22:14:19 -0300

    added note

--- src/main/kotlin/NumbersAndTexts.kt
+++ src/main/kotlin/NumbersAndTexts.kt
@@ -14,3 +14,6 @@ fun main() {
 // Para declarar uma variável do tipo texto basta adicionar entres "aspas" e para números não tem necessidade;
 
 // Explicado também a utilização de comentários, no qual é uma ótima maneira de documentar o seu código;
+
+// OBS: Caso declare uma variável que seja número entre aspas duplas "", o kotlin irá entender este nuúmero como String..
+// Ex.: "1" + "1" não será igual à 2 e sim igual à 11, pois o Kotlin está unindo Strings.
