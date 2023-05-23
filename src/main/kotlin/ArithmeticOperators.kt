a44464a179090de2668d61ff58b12aa3e0f3cbfd
Pedro Moura <ph.rodriguesmoura@gmail.com>
2023-05-23 20:42:01 -0300

    lesson on arithmetic operators

---
+++ src/main/kotlin/ArithmeticOperators.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val a = 10
+    val b = 3
+
+// Addition
+    val soma = a + b  // Resultado: 13
+
+// Subtraction
+    val diferenca = a - b  // Resultado: 7
+
+// Multiplication
+    val produto = a * b  // Resultado: 30
+
+// Division
+    val quociente = a / b  // Resultado: 3
+
+// Module
+    val resto = a % b  // Resultado: 1
+}
+
+// TODO: Explicar o que foi abordado na aula.
+
+// Foi ensinado os operadores básicos da matematica para ser usado na programação Kotlin;
+// Assim como na matemática comum os operadores na programação segue o mesmo padrão com algumas ressalvas, por Ex.:
+// Operador de divisão é composto por uma barra / e o operador de multiplicação pelo sinal de asterisco *.
