bc3ca2d8d11a48a0fe5c5bc2efe0c6d0ceecff98
Pedro Moura <ph.rodriguesmoura@gmail.com>
2023-05-22 21:05:07 -0300

    lesson on numbers and texts

---
+++ src/main/kotlin/NumbersAndTexts.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val name = "Pedro"
+    val age = 22
+    val product = "iMac"
+
+    println(name)
+    println(age)
+    println(product)
+}
+
+// TODO: Explicar o que foi abordado na aula.
+
+// Nesta aula foi introduzido a declaração de números e como diferenciar a declaração de textos e numerais;
+// Para declarar uma variável do tipo texto basta adicionar entres "aspas" e para números não tem necessidade;
+
+// Explicado também a utilização de comentários, no qual é uma ótima maneira de documentar o seu código;
