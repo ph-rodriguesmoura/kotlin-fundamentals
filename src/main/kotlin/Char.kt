74659602cbd9302854882550262f1ff18433e1dc
Pedro Moura <ph.rodriguesmoura@gmail.com>
2023-05-23 21:41:39 -0300

    lesson on special characters

---
+++ src/main/kotlin/Char.kt
@@ -0,0 +1,18 @@
+fun main() {
+    // string
+    val name = "Pedro"
+    val lastName = "Moura"
+    val lastChar = '\n'
+    val firstChar = '\t'
+    println(name + lastChar + lastName)
+    println(name + firstChar + lastName)
+}
+
+// TODO: Explicar o que foi abordado na aula.
+
+// Foi ensinado à utilização do 'Char' um caracter único utilizado bastante em criptografia de senhas por exemplo..
+// a maneira explicita para se declarar um char é pelo uso das aspas simples '', diferente da string que é composta..
+// por uma sequência de "chars" que é declarada com o uso das aspas duplas.
+
+// Existe também os caracter especiais, por exemplo o \n e \t, no qual o \n quando o código chegar neste caracter..
+// será salta uma linha, e o \t será dado um espaço de mais o menos 2 à 4 caracters, tipo muito utilizado em docs.
