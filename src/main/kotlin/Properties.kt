7ddb0652bd9c85f84a36401199411d0eef50c581
Pedro Moura <ph.rodriguesmoura@gmail.com>
2023-05-23 22:00:48 -0300

    lesson on properties

---
+++ src/main/kotlin/Properties.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val name = "Pedro Moura"
+
+    // Função que muda o comportamento de um objeto (String)
+    val newName = name.uppercase()
+
+    // Acessa a propriedade de um objeto (String)
+    val numberName = name.length
+
+    println(newName)
+    println(numberName)
+
+}
+
+// TODO: Explicar o que foi abordado na aula.
+
+// Ensinado como utilizar funções e propriedades de um OBJETO (TIPO), no qual possui para várias finalidades..
+// por Ex.: .length() serve para informar quantos caracteres possui em determinado objeto e o .uppercase()..
+// tem como proposito deixar todas as letras maiusculas do objeto STRING.
+
+// Propriedades tem como principal objetivo utilizar maneiras de mudar o comportamento do objeto e acessar..
+// propriedades que podem ser úteis para o desenvolvedor.
+
+// OBS: OBJETO é referente ao tipo da variável por Ex.: tipo String, Boolean e etc..
