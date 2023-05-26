224d5c48a00d6e40bc2183362ce906b8ee759684
Pedro Moura <ph.rodriguesmoura@gmail.com>
2023-05-25 23:15:01 -0300

    finishing lesson on function

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -1,3 +1,5 @@
+import kotlin.system.exitProcess
+
 fun main() {
     // call - Chamando uma função
     helloWorld()
@@ -23,6 +25,16 @@ fun main() {
 
     // Funções com parametrizações, ao chamar ela é necessário informar seu valor;
     ageValidation(17)
+
+    // Função com parametrização e sem tipo de retorno, porém com argumentos nomeados
+    isAdmin(name = "Pedro", age = 22, isAdmin = true)
+
+    // Funções repetidas com argumentos diferentes
+    loginValidation("email@teste.com", 123456, true)
+    loginValidation("email@teste.com", 123458)
+
+    // Função com valor padrão definido
+    userValidation(123456)
 }
 
 // Função sem retorno
@@ -60,6 +72,37 @@ fun ageValidation(age: Int) {
     }
 }
 
+// Argumentos nomeados
+fun isAdmin(name: String, age: Int, isAdmin: Boolean) {
+    if (age > 18) {
+        println("Olá meu nome é $name, tenho $age e sou admin: $isAdmin")
+    } else {
+        println("Olá meu nome é $name, tenho $age e sou admin: $isAdmin")
+    }
+}
+
+// Sobrecarga
+fun loginValidation(email: String, passaword: Int, isAdmin: Boolean) {
+    if (email === "email@teste.com" && passaword === 123456 && isAdmin == true) {
+        println("Login ao Painel Administrativo Efetuado")
+    } else {
+        println("Acesso Negado")
+    }
+}
+
+fun loginValidation(email: String, passaword: Int) {
+    if (email === "email@teste.com" && passaword === 123456) {
+        println("Login ao Painel Administrativo Efetuado")
+    } else {
+        println("Acesso Negado")
+    }
+}
+
+// Valores padrões
+fun userValidation(passaword: Int, email: String = "teste@teste.com") {
+  println("Seu email é: $email e sua senha: $passaword")
+}
+
 // TODO: Explicar o que foi abordado na aula.
 
 // Foi ensinado que fumções tem o objetivo de organizar o código e reutilizar seus blocos de código quando desejar.
@@ -70,3 +113,11 @@ fun ageValidation(age: Int) {
 // O escopo de uma função é delimitado pelas chaves {}, ou seja uma função será tudo aquilo que contém dentro das {}.
 
 // Funções também podem ter parametrizações ou não, no qual será inserida nos parenteses ().
+
+// Ao chamar uma função que possui parâmetros, para ficar mais facil a compreensão da visualização e compreensão..
+// dos argumentos a serem passados é possível nomear os argumentos.
+
+// É possível repetir o nome da função para criar outra, porém os argumentos não podem ser os mesmos.
+
+// Em kotlin dentro dos parâmetros das funções é possível deixar um valor default, como por exemplo todos os clientes..
+// novos cadastrados terá a senha 123456.
